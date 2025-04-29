78166058581f1abd8b08098765d611501f5b345a
Yuxiang Gong <gongyuxiang@bytedance.com>
2025-04-29 11:48:02 +0800

    build(scm): add release branch name format pkg version

--- python/build_scm.sh
+++ python/build_scm.sh
@@ -5,7 +5,26 @@ ROOT_DIR=$(pwd)
 PYTHON=python3
 PIP="$PYTHON -m pip"
 BUILD_TIME=$(date +%Y%m%d%H%M)
-VERSION_SUFFIX=+byted.${BUILD_TIME}
+# 获取当前分支名，并将特殊字符转换为下划线
+BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+echo "BRANCH_NAME: $BRANCH_NAME"
+
+# 如果分支是以 release_ 或 release/ 开头，则将 release_ 或 release/ 替换为空
+if [[ $BRANCH_NAME =~ ^release[\/_] ]]; then
+    BRANCH_NAME=${BRANCH_NAME#release}
+    BRANCH_NAME=${BRANCH_NAME#/}
+    BRANCH_NAME=${BRANCH_NAME#_}
+    # 如果分支里还有 / ，则将 / 替换为 .
+    BRANCH_NAME=${BRANCH_NAME//\//.}
+    if [[ ! -z $BRANCH_NAME ]]; then
+        BRANCH_NAME=.${BRANCH_NAME}
+    fi
+    VERSION_SUFFIX=+byted${BRANCH_NAME}.${BUILD_TIME}
+else
+    VERSION_SUFFIX=+byted.${BUILD_TIME}
+fi
+
+echo "VERSION_SUFFIX: $VERSION_SUFFIX"
 
 TOS_UTIL_URL=https://tos-tools.tos-cn-beijing.volces.com/linux/amd64/tosutil
 if [ ! -z "$CUSTOM_TOS_UTIL_URL" ]; then
