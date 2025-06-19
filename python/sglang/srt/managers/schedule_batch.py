cfaf40d20d2db1e8ed07b81ddeb4f5f5b24f6415
jinxin.tao <jinxin.tao@bytedance.com>
2025-06-18 17:48:29 -0700

    fix num_gpu derivation

--- python/sglang/srt/managers/schedule_batch.py
+++ python/sglang/srt/managers/schedule_batch.py
@@ -98,6 +98,7 @@ GLOBAL_SERVER_ARGS_KEYS = [
     "triton_attention_reduce_in_fp32",
     "ep_num_redundant_experts",
     "mm_attention_backend",
+    "tp_size"
 ]
 
 # Put some global args for easy access
