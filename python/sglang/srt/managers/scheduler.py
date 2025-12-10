918c69c2398ec8d1179831fe622dcfc027d68072
hanhan.hank <hanhan.hank@bytedance.com>
2025-12-10 15:50:53 +0800

    fix

--- python/sglang/srt/managers/scheduler.py
+++ python/sglang/srt/managers/scheduler.py
@@ -164,8 +164,8 @@ from sglang.srt.mem_cache.eic_hiradix_cache import (
     EICHiRadixCache,
     EICHiRadixCacheBuilder,
     EICPagedHiRadixCache,
-    MambaRadixCache,
 )
+from sglang.srt.mem_cache.mamba_radix_cache import MambaRadixCache
 from sglang.srt.mem_cache.radix_cache import RadixCache
 from sglang.srt.model_executor.forward_batch_info import ForwardMode, PPProxyTensors
 from sglang.srt.multiplex.multiplexing_mixin import SchedulerMultiplexMixin
