3422bbfb7b0bcf0029c044046ce55d0dad67cf59
Hank Han <hanhan7630@outlook.com>
2025-12-10 15:39:59 +0800

    fp8 dpsk r1 (#340)

--- python/sglang/srt/entrypoints/engine.py
+++ python/sglang/srt/entrypoints/engine.py
@@ -845,6 +845,7 @@ def _launch_subprocesses(
                             pp_rank,
                             None,
                             writer,
+                            None,
                         ),
                     )
                     with memory_saver_adapter.configure_subprocess(), numa_utils.configure_subprocess(
