01dde44e4bceb1049ce22a4d0bf873042e13b0d5
zhuxingxin.0 <zhuxingxin.0@bytedance.com>
2026-01-23 14:09:50 +0800

    fix(sgl-python): revert fallback_version

--- python/sglang/version.py
+++ python/sglang/version.py
@@ -2,5 +2,5 @@ try:
     from sglang._version import __version__, __version_tuple__
 except ImportError:
     # Fallback for development without build
-    __version__ = "v0.5.7"
-    __version_tuple__ = (0, 5, 7)
+    __version__ = "0.0.0.dev0"
+    __version_tuple__ = (0, 0, 0, "dev0")
