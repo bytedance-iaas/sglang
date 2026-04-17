10b49a302dbca08ed7fb3c27b1ee58e2eab0e1ee
zhangxiaolei <zhangxiaolei.666@bytedance.com>
2026-04-17 16:14:12 +0800

    Update kernel_direct.h

--- python/sglang/jit_kernel/csrc/gemm/marlin_moe/kernel_direct.h
+++ python/sglang/jit_kernel/csrc/gemm/marlin_moe/kernel_direct.h
@@ -3,12 +3,18 @@
 
 #include "../marlin/marlin.cuh"
 #include "../marlin/marlin_dtypes.cuh"
+#include "marlin_tma_utils.h"
 
 #define MARLIN_DIRECT_KERNEL_PARAMS                                                                       \
   const int4 *__restrict__ A, const int4 *__restrict__ B, int4 *__restrict__ C, int4 *__restrict__ C_tmp, \
       const int4 *__restrict__ b_bias_ptr, const int4 *__restrict__ scales_ptr,                            \
       const uint16_t *__restrict__ scale2_ptr, const int4 *__restrict__ zp_ptr, const int *__restrict__ g_idx, \
-      const int32_t *__restrict__ masked_m_ptr, int expected_m, int a_expert_stride, int num_groups, int prob_m, int prob_n, int prob_k,       \
+      const int32_t *__restrict__ masked_m_ptr, const int32_t *__restrict__ active_expert_ids_ptr,         \
+      const int32_t *__restrict__ active_expert_count_ptr,                                                  \
+      const __grid_constant__ device::marlin_moe::tma::LocalTensorMapDesc tensor_map_b_desc,               \
+      bool has_tensor_map_b_desc, const __grid_constant__ device::marlin_moe::tma::LocalTensorMapDesc tensor_map_s_desc, \
+      bool has_tensor_map_s_desc, int expected_m,                                                           \
+      int a_expert_stride, int c_expert_stride, int num_groups, int prob_m, int prob_n, int prob_k,       \
       int *locks, bool has_bias, bool use_atomic_add, bool use_fp32_reduce, int max_shared_mem
 
 namespace device::marlin_moe_direct {
