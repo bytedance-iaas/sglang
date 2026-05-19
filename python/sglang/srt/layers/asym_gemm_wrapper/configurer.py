1aaab4c54b3662ddc36ba852248a12cfc2f97949
chenqianfzh <51831990+chenqianfzh@users.noreply.github.com>
2026-05-19 15:55:15 -0700

    support sm89 AsymGemm (#487)

--- python/sglang/srt/layers/asym_gemm_wrapper/configurer.py
+++ python/sglang/srt/layers/asym_gemm_wrapper/configurer.py
@@ -1,14 +1,15 @@
 import logging
+import os
 
 from sglang.srt.environ import envs
-from sglang.srt.utils import get_device_sm, is_blackwell_supported
+from sglang.srt.utils import get_device_sm, is_blackwell_supported, is_sm89_supported
 
 logger = logging.getLogger(__name__)
 
 
 def _compute_enable_asym_gemm():
     sm_version = get_device_sm()
-    if sm_version < 90:
+    if sm_version < 89:
         return False
 
     try:
@@ -23,3 +24,7 @@ ENABLE_JIT_ASYMGEMM = _compute_enable_asym_gemm()
 
 ASYMGEMM_BLACKWELL = ENABLE_JIT_ASYMGEMM and is_blackwell_supported()
 ASYMGEMM_SCALE_UE8M0 = ASYMGEMM_BLACKWELL
+ASYMGEMM_SM89 = ENABLE_JIT_ASYMGEMM and (
+    (is_sm89_supported() and not ASYMGEMM_BLACKWELL)
+    or os.environ.get("SGLANG_ASYMGEMM_SM89", "0") == "1"
+)
