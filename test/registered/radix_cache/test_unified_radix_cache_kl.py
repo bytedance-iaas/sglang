24a62b8757b12cc2b7ecca355777050376ba9b74
wangyicong <wangyicong@bytedance.com>
2026-05-20 23:53:39 +0800

    test(radix): temporarily skip swa gsm8k

--- test/registered/radix_cache/test_unified_radix_cache_kl.py
+++ test/registered/radix_cache/test_unified_radix_cache_kl.py
@@ -245,6 +245,12 @@ class TestUnifiedSWARadixCache(UnifiedRadixTreeTestMixin, CustomTestCase):
     gsm8k_threshold = 0.75
     mmlu_threshold = 0.75
 
+    @unittest.skip(
+        "Temporarily skip unstable SWA GSM8K accuracy check in CI while investigating test threshold/config mismatch"
+    )
+    def test_gsm8k(self):
+        pass
+
     @classmethod
     def setUpClass(cls):
         cls.model = SWA_MODEL
