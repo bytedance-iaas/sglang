7801c88997b9017d6ca5fc41809e4cdf4aa44c24
wanghanpei <wanghanpei@bytedance.com>
2026-05-22 23:43:15 +0800

    add max_shape and warning

--- python/sglang/srt/environ.py
+++ python/sglang/srt/environ.py
@@ -531,7 +531,7 @@ class Envs:
     SGLANG_MM_PRECOMPUTE_HASH = EnvBool(False)
     SGLANG_VIT_ENABLE_CUDA_GRAPH = EnvBool(False)
     SGLANG_VIT_PACK = EnvBool(False)
-    SGLANG_VIT_INFER_TIMES = EnvInt(1)
+    SGLANG_VIT_INFER_MAX_SHAPE = EnvInt(0)
     SGLANG_MM_SKIP_COMPUTE_HASH = EnvBool(False)
 
 
