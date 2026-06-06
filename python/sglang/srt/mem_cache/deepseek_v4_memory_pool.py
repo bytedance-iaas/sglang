7fff18c6ff718664053ad29585480bb30c530fca
zhangxiaolei <zhangxiaolei.666@bytedance.com>
2026-06-06 17:42:14 +0800

    Update deepseek_v4_memory_pool.py

--- python/sglang/srt/mem_cache/deepseek_v4_memory_pool.py
+++ python/sglang/srt/mem_cache/deepseek_v4_memory_pool.py
@@ -9,7 +9,7 @@ import torch
 from sglang.jit_kernel.dsv4 import fused_k_norm_rope_flashmla, fused_store_cache
 from sglang.srt.constants import GPU_MEMORY_TYPE_KV_CACHE
 from sglang.srt.environ import envs
-from sglang.srt.layers.attention.dsa import index_buf_accessor
+from sglang.srt.layers.attention.nsa import index_buf_accessor
 from sglang.srt.layers.attention.dsv4 import (
     index_buf_accessor as dsv4_index_buf_accessor,
 )
