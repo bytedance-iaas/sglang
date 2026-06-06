6ecfd5cff249516fe899b7806b674856bb6a64a9
zhangxiaolei <zhangxiaolei.666@bytedance.com>
2026-06-06 20:50:15 +0800

    Update __init__.py

--- python/sglang/jit_kernel/dsv4/__init__.py
+++ python/sglang/jit_kernel/dsv4/__init__.py
@@ -1,5 +1,9 @@
 from .compress import *
 from sglang.jit_kernel.deepseek_v4 import (
+    CompressorDecodePlan as legacy_CompressorDecodePlan,
+    CompressorPrefillPlan as legacy_CompressorPrefillPlan,
+    compress_forward as legacy_compress_forward,
+    compress_fused_norm_rope_inplace,
     fused_k_norm_rope_flashmla,
     fused_store_cache,
     linear_bf16_fp32,
@@ -13,9 +17,13 @@ __all__ = [
     "CompressorDecodePlan",
     "CompressorPrefillPlan",
     "compress_forward",
+    "compress_fused_norm_rope_inplace",
     "compress_norm_rope_store",
     "fused_k_norm_rope_flashmla",
     "fused_store_cache",
+    "legacy_CompressorDecodePlan",
+    "legacy_CompressorPrefillPlan",
+    "legacy_compress_forward",
     "linear_bf16_fp32",
     "make_name",
     "mega_moe_pre_dispatch_sm90",
