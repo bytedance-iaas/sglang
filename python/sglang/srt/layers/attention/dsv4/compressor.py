2f5aa2dc32c446ea3db7c0616b1c9d31c5a3130f
zhangxiaolei <zhangxiaolei.666@bytedance.com>
2026-06-14 16:35:02 +0800

    Replicate DSV4 DCP C4 indexer cache

--- python/sglang/srt/layers/attention/dsv4/compressor.py
+++ python/sglang/srt/layers/attention/dsv4/compressor.py
@@ -244,7 +244,6 @@ class CompressorBackendMixin:
                 layer_id=layer_id,
                 loc=self.forward_metadata.core_metadata.c4_out_loc,
                 cache_k=new_compressed_kv,
-                dcp_kv_mask=forward_batch.dcp_kv_mask,
             )
         else:
             new_compressed_kv_fp8, new_compressed_kv_scale = act_quant(
@@ -255,7 +254,6 @@ class CompressorBackendMixin:
                 loc=self.forward_metadata.core_metadata.c4_out_loc,
                 index_k=new_compressed_kv_fp8,
                 index_k_scale=new_compressed_kv_scale,
-                dcp_kv_mask=forward_batch.dcp_kv_mask,
             )
 
 
