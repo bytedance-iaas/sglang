db70d61dd770ef429c104aad43187796966fc73d
zhangxiaolei <zhangxiaolei.666@bytedance.com>
2026-09-17 22:17:23 +0800

    Increase grouped indexer warpgroup parallelism

--- python/sglang/kernels/jit/csrc/sm90_fp4_grouped_indexer/kernel.cuh
+++ python/sglang/kernels/jit/csrc/sm90_fp4_grouped_indexer/kernel.cuh
@@ -85,7 +85,7 @@ struct Sm90Fp4GroupedIndexerKernel {
   static constexpr int BLOCK_L = 64;
   static constexpr int SCALE_GROUPS = 4;
   static constexpr int SCALE_GROUP_SIZE = 32;
-  static constexpr int NUM_WARPGROUPS = 2;
+  static constexpr int NUM_WARPGROUPS = 4;
   static constexpr int NUM_THREADS = 128 * NUM_WARPGROUPS;
 
   using SmemLayout =
